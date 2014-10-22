c00d47bf6cb436614d966deeeded8e08bb5a49f5
Carsten Richter <carsten.richter@physik.tu-freiberg.de>
2014-10-22 12:20:12 +0200

    added example for fitls

---
+++ examples/fitls/fitexample.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#----------------------------------------------------------------------
+# Description:
+# Author: Carsten Richter <carsten.richter@desy.de>
+# Created at: Mi 22. Okt 11:52:11 CEST 2014
+# Computer: haso227r 
+# System: Linux 3.13.0-37-generic on x86_64
+#
+# Copyright (c) 2014 Carsten Richter  All rights reserved.
+#----------------------------------------------------------------------
+import pylab as pl
+import evaluationtools as et
+
+
+
+def twogauss(x, cen1, cen2, ymax1, ymax2, w1, w2, offset):
+    return et.gaussian(x, cen1, ymax1, w1) \
+         + et.gaussian(x, cen2, ymax2, w2) \
+         + offset
+
+
+
+realparam = dict(cen1=-1, cen2=0.9, ymax1=2, ymax2=3, w1=0.5, w2=.8, offset=1)
+
+x_m = pl.linspace(-5, 5, 501)
+y_m = twogauss(x_m, **realparam) + 0.2*pl.rand(len(x_m)) # adding some noise
+pl.plot(x_m, y_m, ".k", label="data", ms=2)
+
+guess = dict((k,2*pl.rand()-1) for k in realparam) # random start values
+
+fit = et.fitls(x_m, y_m, twogauss, guess, fitalg="simplex")
+pl.plot(x_m, fit.ysim, "-r", label="simplex")
+print("Fit results simplex: %f"%fit.err)
+print(fit.popt)
+
+
+fit = et.fitls(x_m, y_m, twogauss, guess, fitalg="leastsq")
+pl.plot(x_m, fit.ysim, "-g", dashes=(5,2), label="leastsq")
+print("Fit results leastsq: %f"%fit.err)
+print(fit.popt)
+pl.legend()
+pl.show()
