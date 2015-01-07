c985b4daf751300c071983934be61a0a738f0e11
Carsten Richter <carsten.richter@physik.tu-freiberg.de>
2015-01-07 17:19:36 +0100

    added convenience functions for user input

--- evaluationtools/diverse.py
+++ evaluationtools/diverse.py
@@ -7,6 +7,7 @@
 
 import numpy as np
 import os
+import sys
 from scipy import ndimage, optimize, special
 import time
 import scantype
@@ -278,6 +279,64 @@ def PolynomialFit(x, y, anchors=None, avgrange=0, order=2, indf=None):
         poly = sum([vopt[i]*x**(N - 1 - i) for i in range(N)])
     
     return poly
+
+def yesno(question, default="yes"):
+    """
+    From Recipe 577058: http://code.activestate.com/recipes/577058/
+    Ask a yes/no question via raw_input() and return their answer.
+    
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":"yes",   "y":"yes",  "ye":"yes",
+             "no":"no",     "n":"no"}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while 1:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return default
+        elif choice in valid.keys():
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
+
+def ask(question, default="", default_factory=str):
+    """
+    Ask a question via raw_input() and return their answer.
+    
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+    """
+    if default != "":
+        prompt = " [%s] "%str(default)
+    else:
+        prompt = " ? "
+
+    sys.stdout.write(question + prompt)
+    try:
+        answer = default_factory(raw_input())
+    except:
+        return default
+    if answer==default_factory():
+        return default
+    else:
+        return answer
+    
 
 
 class TempExpansion(object):
