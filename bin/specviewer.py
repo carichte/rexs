88299eca2bc44cd8249912746068c6275a92e188
Carsten Richter <carsten.richter@physik.tu-freiberg.de>
2015-03-06 14:59:09 +0100

    cleaned comments

--- bin/specviewer.py
+++ bin/specviewer.py
@@ -72,19 +72,19 @@ if not os.path.isfile(conffile):
     except:
         pass
 
-def md5_for_file(path, block_size=256*128, hr=False):
-    '''
-    Block size directly depends on the block size of your filesystem
-    to avoid performances issues
-    Here I have blocks of 4096 octets (Default NTFS)
-    '''
-    md5 = hashlib.md5()
-    with open(path,'rb') as f: 
-        for chunk in iter(lambda: f.read(block_size), b''): 
-             md5.update(chunk)
-    if hr:
-        return md5.hexdigest()
-    return md5.digest()
+#def md5_for_file(path, block_size=256*128, hr=False):
+#    '''
+#    Block size directly depends on the block size of your filesystem
+#    to avoid performances issues
+#    Here I have blocks of 4096 octets (Default NTFS)
+#    '''
+#    md5 = hashlib.md5()
+#    with open(path,'rb') as f: 
+#        for chunk in iter(lambda: f.read(block_size), b''): 
+#             md5.update(chunk)
+#    if hr:
+#        return md5.hexdigest()
+#    return md5.digest()
 
 # READING CONFIG ##########################################################
 if os.path.isfile(conffile):
